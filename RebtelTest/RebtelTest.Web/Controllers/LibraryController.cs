9e99b6fae5f2415f164662cd4707ec6df37c212c
Haritha Tennakoon <haritha.tennakoon@protendering.com>
2022-02-28 00:24:05 +0530

    Added API project

---
+++ RebtelTest/RebtelTest.Web/Controllers/LibraryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RebtelTest.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LibraryController : ControllerBase
+    {
+        [HttpGet("books/most-borrowed")]
+        public async Task<string> GetMostBorrowedBooks()
+        {
+            return "";
+        }
+    }
+}
