fea6227d96943b8a0ecf7d5e8fa7bad84f50b12d
Haritha Tennakoon <haritha.tennakoon@protendering.com>
2022-03-01 17:59:58 +0530

    Added Starters

---
+++ RebtelTest/Rebtel.Starters/Program.cs
@@ -0,0 +1,15 @@
+﻿using Rebtel.Starters.Questions;
+
+namespace Rebtel.Starters
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Question 1
+            var question1 = new Question1();
+            bool isPowerOfTwoTest1 = question1.IsPowerOfTwo(64); // returns true
+            bool isPowerOfTwoTest2 = question1.IsPowerOfTwo(65); // returns false
+        }
+    }
+}
---
+++ RebtelTest/Rebtel.Starters/Questions/Question1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Rebtel.Starters.Questions
+{
+    public sealed class Question1
+    {
+        public bool IsPowerOfTwo(int number)
+        {
+            if (number < 2)
+            {
+                return false;
+            }
+
+            double logOfNumber = Math.Log(number);
+            double logOfTow = Math.Log(2);
+            double divide = logOfNumber / logOfTow;
+
+            var upperValue = Math.Ceiling(divide);
+            var lowerValue = Math.Floor(divide);
+
+            return upperValue == lowerValue;
+        }
+    }
+}
